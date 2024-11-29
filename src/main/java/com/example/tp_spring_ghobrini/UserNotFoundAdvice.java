e7e326136526fecfe50be89b0f59a18c287f0a52
sofianghobrini <sofiangho31000@outlook.fr>
2024-11-29 17:20:24 +0100

    TP2 UserNotFoundAdvice

---
+++ src/main/java/com/example/tp_spring_ghobrini/UserNotFoundAdvice.java
@@ -0,0 +1,14 @@
+package com.example.tp_spring_ghobrini;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestControllerAdvice
+public class UserNotFoundAdvice {
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String employeeNotFoundHandler(UserNotFoundException ex) {
+        return ex.getMessage();
+    }
+}
+
