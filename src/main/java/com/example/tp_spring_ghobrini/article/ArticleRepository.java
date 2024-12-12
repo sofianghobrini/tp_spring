25777e24f0ab6879d5aba7b45a4ae1f4dff6f676
sofianghobrini <sofiangho31000@outlook.fr>
2024-12-12 14:29:58 +0100

    pas de changement

--- src/main/java/com/example/tp_spring_ghobrini/article/ArticleRepository.java
+++ src/main/java/com/example/tp_spring_ghobrini/article/ArticleRepository.java
@@ -1,7 +1,9 @@
 package com.example.tp_spring_ghobrini.article;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.Optional;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-
 }
